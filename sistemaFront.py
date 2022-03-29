0d0876e9bb76a437a79e4523e3fb9d9c9188dea3
maxbarros <mbds.snf19@uea.edu.br>
2022-03-29 00:52:10 -0400

    commit

--- sistemaFront.py
+++ sistemaFront.py
@@ -1,9 +1,19 @@
 from pywebio import *
-
+from pywebio.output import *
 
 def main():  # PyWebIO application function
-    output.put_markdown(r""" # MovieRec """)
-    output.put_text("Protótipo de Sistema de Recomendação de filmes")
+    put_markdown(r""" # MovieRec """)
+    put_text("Protótipo de Sistema de Recomendação de filmes")
+
+    put_table([
+    ['Type', 'Content'],
+    ['html', put_html('X<sup>2</sup>')],
+    ['text', '<hr/>'],  # equal to ['text', put_text('<hr/>')]
+    ['buttons', put_buttons(['A', 'B'], onclick=...)],  
+    ['markdown', put_markdown('`Awesome PyWebIO!`')],
+    ['file', put_file('hello.text', b'hello world')],
+    ['table', put_table([['A', 'B'], ['C', 'D']])]
+    ])
 
 
 if __name__ == '__main__':
