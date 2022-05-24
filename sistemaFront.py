8061f81081f8a876dab87943224c8c5bf102c3eb
Paulo André <pauloacf148@gmail.com>
2022-05-23 22:52:18 -0400

    Adicionando novo usuário

--- sistemaFront.py
+++ sistemaFront.py
@@ -1,10 +1,19 @@
 import csv
 from pywebio import *
+from pywebio.input import TEXT
 from pywebio.output import *
 from sistema_recomedacao import *
 
 Users = []
 Movie_list = []
+
+def addNewUser():
+    new_user = input.input("Nome do usuário：", type=TEXT)
+    Users.append(new_user)
+    with open('usuarios.csv', 'w', newline='', encoding='utf-8') as file:
+        writer = csv.writer(file)
+        for user in Users:    
+            writer.writerow([user])
 
 with open('usuarios.csv', mode='r', encoding='utf-8') as arq:
     leitor = csv.reader(arq, delimiter=',')
@@ -17,6 +26,7 @@ with open('filmes.csv', mode='r', encoding='utf-8') as arq:
         Movie_list.append(coluna[0])
 
 def main():  # PyWebIO application function
+    addNewUser()
     username = input.select('Selecionar Usuário', Users)
     movie = input.select('Escolha um filme para avaliar', Movie_list)
     nota = input.select('Qual nota você dá para esse filme?', [0, 1, 2, 3, 4, 5])
